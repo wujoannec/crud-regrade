789bc33d172f70a20a2d9ebc72cb4dfeb7fdb824
wujoannec <81791252+wujoannec@users.noreply.github.com>
2023-03-17 23:58:45 -0700

    Finished LocalStorage

--- blog.js
+++ blog.js
@@ -4,6 +4,7 @@ export function repopulateTable() {
 	//Populate table with data from localstorage 
 	let count2 = 0;
 	let tableBody = document.querySelector("#blog-posts").getElementsByTagName('tbody')[0];
+	removal();
 	Object.keys(localStorage).forEach((key) => {
 		let newRow = tableBody.insertRow(-1);
 		let col0 = newRow.insertCell(0);
@@ -24,55 +25,50 @@ export function repopulateTable() {
 	});
 }
 
-
-export function crudOnSubmit() {
-    const form = document.getElementById("formy");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        if (nonEmptyTitle()) {
-            let formData = {};
-            formData["title"] = document.querySelector("#title").value;
-            formData["date"] = document.querySelector("#date").value;
-            formData["summary"] = document.querySelector("#summary").value;
-            if (selectedRowToEdit != null){
-                updatePost(formData);
-            }
-            else{
-                createPost(formData);
-            }
-            repopulateTable();
-            blankForm();
-        }
-    })
+function removal(){
+	let table = document.querySelector("#blog-posts")
+	while(table.rows.length > 0) {
+		table.deleteRow(0);
+	}
 }
 
+export function crudOnSubmit() {
+	const form = document.getElementById("formy");
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		if (nonEmptyTitle()) {
+			let formData = {};
+			formData["title"] = document.querySelector("#title").value;
+			formData["date"] = document.querySelector("#date").value;
+			formData["summary"] = document.querySelector("#summary").value;
+			if (selectedRowToEdit != null){
+				updatePost(formData);
+			}
+			else{
+				createPost(formData);
+			}
+			repopulateTable();
+			blankForm();
+		}
+	})
+}
 
 function nonEmptyTitle() {
-    //If title exists in localstorage -> error + return
     let nonEmptyTitle = true;
     let formTitle = document.querySelector("#title");
     formTitle.value == "" ? (nonEmptyTitle = false, alert("Invalid: Must input title")):(nonEmptyTitle = true);
     return nonEmptyTitle;
 }
 
-function createPost(data) {
-    
-    localStorage.setItem('uwu', JSON.stringify(datesum));
-    col0.innerHTML = data.title;
-    col1.innerHTML = data.date;
-    col2.innerHTML = data.summary;
-    localStorage.setItem(key, value);
+function createPost(formData) {
+	let arr = [formData.date, formData.summary];
+	localStorage.setItem(formData.title, JSON.stringify(arr));
 }
 
 function updatePost(formData) {
-    let col0 = selectedRowToEdit.cells[0];
-    col0.innerHTML = formData.title;
-    let col1 = selectedRowToEdit.cells[1];
-    col1.innerHTML = formData.date;
-    let col2 = selectedRowToEdit.cells[2];
-    col2.innerHTML = formData.summary;
+	localStorage.removeItem(selectedRowToEdit.cells[0]);
+	createPost(formData);
 }
-
 
 function blankForm() {
     let formTitle = document.querySelector("#title");
@@ -85,23 +81,21 @@ function blankForm() {
 }
 
 function fillFormForEdit(selectedEdit) {
-    let selectedRowToEdit = selectedEdit.parentElement.parentElement;
-    let confirmationText= `Edit post titled "${selectedRowToEdit.cells[0].innerHTML}"?\nAll contents of the current form will be replaced.`
-    if (confirm(confirmationText) == true) {
-        document.querySelector("#title").value = selectedRowToEdit.cells[0].innerHTML;
-        document.querySelector("#date").value = selectedRowToEdit.cells[1].innerHTML;
-        document.querySelector("#summary").value = selectedRowToEdit.cells[2].innerHTML;
-    }
+	let selectedRowToEdit = selectedEdit.parentElement.parentElement;
+	let confirmationText= `Edit post titled "${selectedRowToEdit.cells[0].innerHTML}"?\nAll contents of the current form will be replaced.`
+	if (confirm(confirmationText) == true) {
+		document.querySelector("#title").value = selectedRowToEdit.cells[0].innerHTML;
+		document.querySelector("#date").value = selectedRowToEdit.cells[1].innerHTML;
+		document.querySelector("#summary").value = selectedRowToEdit.cells[2].innerHTML;
+	}
 }
 
 
 function remove(selectedRemove) {
-    let rowToDelete = selectedRemove.parentElement.parentElement;
-    let confirmationText= `Delete post titled "${rowToDelete.cells[0].innerHTML}" permanently?`
-    if (confirm(confirmationText) == true) {
-
-        let blogTable = document.querySelector("#blog-posts");
-        blogTable.deleteRow(rowToDelete.rowIndex);
-        count--;
-    }
+	let rowToDelete = selectedRemove.parentElement.parentElement;
+	let confirmationText= `Delete post titled "${rowToDelete.cells[0].innerHTML}" permanently?`
+	if (confirm(confirmationText) == true) {
+		localStorage.removeItem(rowToDelete.cells[0].innerHTML);
+	}
+	repopulateTable();
 }
