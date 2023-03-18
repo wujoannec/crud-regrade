85f560de56d4d02690de3cc4c769756965dbe62d
wujoannec <81791252+wujoannec@users.noreply.github.com>
2023-03-17 23:31:09 -0700

    Halfway LocalStorage Changes

--- blog.js
+++ blog.js
@@ -1,61 +1,75 @@
-let rowToEdit = null;
-let count = 0;
+let selectedRowToEdit = null;
+
+export function repopulateTable() {
+	//Populate table with data from localstorage 
+	let count2 = 0;
+	let tableBody = document.querySelector("#blog-posts").getElementsByTagName('tbody')[0];
+	Object.keys(localStorage).forEach((key) => {
+		let newRow = tableBody.insertRow(-1);
+		let col0 = newRow.insertCell(0);
+		let col1 = newRow.insertCell(1);
+		let col2 = newRow.insertCell(2);
+		let col3 = newRow.insertCell(3);
+		col0.innerHTML = key;
+		col1.innerHTML = JSON.parse(localStorage.getItem(key))[0];
+		col2.innerHTML = JSON.parse(localStorage.getItem(key))[1];
+		col3.innerHTML = 
+		`<a class = "edit-click"><i class="fa-solid fa-pen-to-square"></i></a>
+		<a class = "delete-click"><i class="fa-solid fa-trash"></i></a>`;
+		let editClick = document.getElementsByClassName("edit-click")[count2];
+		editClick.addEventListener("click", ()=> {fillFormForEdit(editClick)});
+		let deleteClick = document.getElementsByClassName("delete-click")[count2];
+		deleteClick.addEventListener("click", ()=> {remove(deleteClick)});
+		count2++;
+	});
+}
+
+
 export function crudOnSubmit() {
     const form = document.getElementById("formy");
     form.addEventListener("submit", (event) => {
         event.preventDefault();
-        if (uniqueTitle()) {
+        if (nonEmptyTitle()) {
             let formData = {};
             formData["title"] = document.querySelector("#title").value;
             formData["date"] = document.querySelector("#date").value;
             formData["summary"] = document.querySelector("#summary").value;
-            if (rowToEdit != null){
+            if (selectedRowToEdit != null){
                 updatePost(formData);
-                blankForm();
             }
             else{
                 createPost(formData);
-                blankForm();
             }
+            repopulateTable();
+            blankForm();
         }
     })
 }
 
 
-function uniqueTitle() {
-    let uniqueTitle = true;
+function nonEmptyTitle() {
+    //If title exists in localstorage -> error + return
+    let nonEmptyTitle = true;
     let formTitle = document.querySelector("#title");
-    formTitle.value == "" ? (uniqueTitle = false, alert("Invalid: Must input title")):(uniqueTitle = true);
-    return uniqueTitle;
+    formTitle.value == "" ? (nonEmptyTitle = false, alert("Invalid: Must input title")):(nonEmptyTitle = true);
+    return nonEmptyTitle;
 }
 
 function createPost(data) {
-    let tableBody = document.querySelector("#blog-posts").getElementsByTagName('tbody')[0];
-    let newRow = tableBody.insertRow(-1);
-    count++;
-    let col0 = newRow.insertCell(0);
-    let col1 = newRow.insertCell(1);
-    let col2 = newRow.insertCell(2);
-    let col3 = newRow.insertCell(3);
+    
+    localStorage.setItem('uwu', JSON.stringify(datesum));
     col0.innerHTML = data.title;
     col1.innerHTML = data.date;
     col2.innerHTML = data.summary;
-    col3.innerHTML = 
-      `<a class = "edit-click"><i class="fa-solid fa-pen-to-square"></i></a>
-      <a class = "delete-click"><i class="fa-solid fa-trash"></i></a>`;
-    let editClick = document.getElementsByClassName("edit-click")[count-1];
-    editClick.addEventListener("click", ()=> {fillFormForEdit(editClick)});
-    let deleteClick = document.getElementsByClassName("delete-click")[count-1];
-    deleteClick.addEventListener("click", ()=> {remove(deleteClick)});
+    localStorage.setItem(key, value);
 }
 
 function updatePost(formData) {
-    console.log("here");
-    let col0 = rowToEdit.cells[0];
+    let col0 = selectedRowToEdit.cells[0];
     col0.innerHTML = formData.title;
-    let col1 = rowToEdit.cells[1];
+    let col1 = selectedRowToEdit.cells[1];
     col1.innerHTML = formData.date;
-    let col2 = rowToEdit.cells[2];
+    let col2 = selectedRowToEdit.cells[2];
     col2.innerHTML = formData.summary;
 }
 
@@ -67,16 +81,16 @@ function blankForm() {
     formDate.value = "";
     let formSummary = document.querySelector("#summary");
     formSummary.value = "";
-    rowToEdit = null;
+    selectedRowToEdit = null;
 }
 
 function fillFormForEdit(selectedEdit) {
-    let rowToEdit = selectedEdit.parentElement.parentElement;
-    let confirmationText= `Edit post titled "${rowToEdit.cells[0].innerHTML}"?\nAll contents of the current form will be replaced.`
+    let selectedRowToEdit = selectedEdit.parentElement.parentElement;
+    let confirmationText= `Edit post titled "${selectedRowToEdit.cells[0].innerHTML}"?\nAll contents of the current form will be replaced.`
     if (confirm(confirmationText) == true) {
-        document.querySelector("#title").value = rowToEdit.cells[0].innerHTML;
-        document.querySelector("#date").value = rowToEdit.cells[1].innerHTML;
-        document.querySelector("#summary").value = rowToEdit.cells[2].innerHTML;
+        document.querySelector("#title").value = selectedRowToEdit.cells[0].innerHTML;
+        document.querySelector("#date").value = selectedRowToEdit.cells[1].innerHTML;
+        document.querySelector("#summary").value = selectedRowToEdit.cells[2].innerHTML;
     }
 }
 
