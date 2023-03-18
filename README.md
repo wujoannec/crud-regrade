71e88cb2527cdc19129e0181beb01a1c26742018
wujoannec <81791252+wujoannec@users.noreply.github.com>
2023-03-17 22:32:18 -0700

    Updates to Functionality

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 # (CRUD) Regrade Request
 Joanne Wu
 A16623203
-
+https://teal-pegasus-7dfe7d.netlify.app/
 Thank you again for allowing me to regain points on this assignment and improve my code.
--- blog.js
+++ blog.js
@@ -1,5 +1,5 @@
-var rowToEdit = null;
-var count = 0;
+let rowToEdit = null;
+let count = 0;
 export function crudOnSubmit() {
     const form = document.getElementById("formy");
     form.addEventListener("submit", (event) => {
@@ -17,7 +17,6 @@ export function crudOnSubmit() {
                 createPost(formData);
                 blankForm();
             }
-            return;
         }
     })
 }
@@ -42,29 +41,24 @@ function createPost(data) {
     col1.innerHTML = data.date;
     col2.innerHTML = data.summary;
     col3.innerHTML = 
-    `&nbsp&nbsp&nbsp<a class = "edit-click"><em><u>Edit</u></em></a>&nbsp&nbsp&nbsp
-    <a class = "delete-click"><em><u>Delete</u></em></a>`;
+      `<a class = "edit-click"><i class="fa-solid fa-pen-to-square"></i></a>
+      <a class = "delete-click"><i class="fa-solid fa-trash"></i></a>`;
     let editClick = document.getElementsByClassName("edit-click")[count-1];
-    editClick.onmouseover = function() {editClick.style.color = "#3598c6";};
-    editClick.onmouseout = function() {editClick.style.color = "black";};
     editClick.addEventListener("click", ()=> {fillFormForEdit(editClick)});
-    
     let deleteClick = document.getElementsByClassName("delete-click")[count-1];
-    deleteClick.onmouseover = function() {deleteClick.style.color = "#3598c6";};
-    deleteClick.onmouseout = function() {deleteClick.style.color = "black";};
     deleteClick.addEventListener("click", ()=> {remove(deleteClick)});
-    return;
 }
 
 function updatePost(formData) {
+    console.log("here");
     let col0 = rowToEdit.cells[0];
     col0.innerHTML = formData.title;
     let col1 = rowToEdit.cells[1];
     col1.innerHTML = formData.date;
     let col2 = rowToEdit.cells[2];
     col2.innerHTML = formData.summary;
-    return;
 }
+
 
 function blankForm() {
     let formTitle = document.querySelector("#title");
@@ -74,25 +68,26 @@ function blankForm() {
     let formSummary = document.querySelector("#summary");
     formSummary.value = "";
     rowToEdit = null;
-    return;
 }
 
 function fillFormForEdit(selectedEdit) {
-    rowToEdit = selectedEdit.parentElement.parentElement;
-    document.querySelector("#title").value = rowToEdit.cells[0].innerHTML;
-    document.querySelector("#date").value = rowToEdit.cells[1].innerHTML;
-    document.querySelector("#summary").value = rowToEdit.cells[2].innerHTML;
-    return;
+    let rowToEdit = selectedEdit.parentElement.parentElement;
+    let confirmationText= `Edit post titled "${rowToEdit.cells[0].innerHTML}"?\nAll contents of the current form will be replaced.`
+    if (confirm(confirmationText) == true) {
+        document.querySelector("#title").value = rowToEdit.cells[0].innerHTML;
+        document.querySelector("#date").value = rowToEdit.cells[1].innerHTML;
+        document.querySelector("#summary").value = rowToEdit.cells[2].innerHTML;
+    }
 }
 
 
 function remove(selectedRemove) {
-    if (confirm('Delete record?') == true) {
-        let rowToDelete = selectedRemove.parentElement.parentElement;
+    let rowToDelete = selectedRemove.parentElement.parentElement;
+    let confirmationText= `Delete post titled "${rowToDelete.cells[0].innerHTML}" permanently?`
+    if (confirm(confirmationText) == true) {
+
         let blogTable = document.querySelector("#blog-posts");
         blogTable.deleteRow(rowToDelete.rowIndex);
         count--;
-        blankForm();
     }
-    return;
 }
